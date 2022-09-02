2490bdce1a79b8d260c83106e5d147d9ecbd34be
Oluwatobi22 <bakareoluwatobi22@gmail.com>
2022-09-02 01:07:09 +0100

    uhujhujh

--- src/component/Background.js
+++ src/component/Background.js
@@ -9,8 +9,8 @@ const Background = () => {
                         <div className="col-md-5 ">
                             <h1 className="text-white fw-bold float-start">Quickteller</h1>
                             <p className="text-start text-white float-start" style={{fontSize:"12px"}}>Digital hub connecting people to seamless payments, global shopping, flights and more..</p>
-                            <p className='text-white'>Email:bakareemmauel2233@gmail.com</p>
-                            <button className="btn btn-md text-white float-start mb-3" style={{ background: "#479fc8"}}><a href="#" className="text-decoration-none"></a><span style={{fontSize:'14px'}}>Sign up on Quickteller</span></button>
+                            
+                            <button className="btn btn-lg text-white float-start mb-3" style={{ background: "#479fc8"}}><a href="#" className="text-decoration-none"></a><span style={{fontSize:'14px'}}>Sign up on Quickteller</span></button>
                         </div>
                     </div>
                 </div>
