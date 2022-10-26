d2c4412bac770154e91cd03e877b3c20d724bfb0
Oluwatobi22 <bakareoluwatobi22@gmail.com>
2022-10-27 00:14:45 +0100

    i added some fuctionality

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Signup from "./Signup";
-import inter from "../assest/image/reinter.png"
+import inter from "../assest/image/reinter.png";
 const NavBar = () => {
   return (
     <>
-      <div class='container' style={{ marginLeft: "-22px" }}>
+      <div class="container" style={{ marginLeft: "-22px" }}>
         <nav class="navbar navbar-expand-lg bg-white ">
           <div class="container-fluid">
             <a class="navbar-brand" style={{ marginLeft: "11vw" }} href="#">
-              <img src="https://www.interswitchgroup.com/assets/images/home/interswitch_logo.svg" style={{ width: '100px' }} className='img-responsive img-fluid' />
+              <img
+                src="https://www.interswitchgroup.com/assets/images/home/interswitch_logo.svg"
+                style={{ width: "100px" }}
+                className="img-responsive img-fluid"
+              />
             </a>
-            <button class="navbar-toggler"
+            <button
+              class="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNav"
@@ -18,7 +23,10 @@ const NavBar = () => {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span class="navbar-toggler-icon top-bar"></span>
+              <span class="navbar-toggler-icon middle-bar"></span>
+              <span class="navbar-toggler-icon bottom-bar"></span>
+
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ms-4 ">
@@ -52,14 +60,20 @@ const NavBar = () => {
           </div>
         </nav>
       </div>
-      <div className="container-fluid" style={{ backgroundColor: '#18425d', height: "8vh", }} >
+      <div
+        className="container-fluid"
+        style={{ backgroundColor: "#18425d", height: "8vh" }}>
         <div className="container">
           <nav class="navbar py-3" style={{ marginLeft: "3vw" }}>
             <div class="container-fluid">
               <span class="navbar-text">
                 <h6 className="text-white">Quickteller</h6>
               </span>
-              <span><p className="text-white">Interested? <span className="fw-bold">Get Started</span></p></span>
+              <span>
+                <p className="text-white">
+                  Interested? <span className="fw-bold">Get Started</span>
+                </p>
+              </span>
             </div>
           </nav>
         </div>
