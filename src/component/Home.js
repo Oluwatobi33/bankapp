bda1db5d5d7b945bd194bdc1f2697d7fea02f473
Oluwatobi33 <bakareoluwatobi22@gmail.com>
2023-03-02 01:09:32 +0100

    I made changes to the home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,20 +8,40 @@ import FootPrint from './FootPrint'
 import NavBar from './NavBar'
 import Product from './Product'
 import Shop from './Shop'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
+  const [backTop, setbackTop] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setbackTop(true)
+      } else {
+        setbackTop(false)
+      }
+    })
+  }, [])
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
   return (
     <>
-    <NavBar/> 
-     <Background/>
-      <Every/>
-       <Shop/>
-       <Download/>
-       <ExistingCust/> 
-       <Product/>
-        <FootPrint/>
-        <Footer/>
-    
+      <NavBar />
+      <Background />
+      <Every />
+      <Shop />
+      <Download />
+      <ExistingCust />
+      <Product />
+      <FootPrint />
+      <Footer />
+      {Home && (
+        <button className='border-0' style={{ position: "fixed", backgroundColor: "#18425D", color: "#FFFFFF", bottom: "100px", right: "50px", height: "50px", width: "50px", fontSize: "40px" }} onClick={scrollUp}>^</button>
+      )}
     </>
   )
 }
